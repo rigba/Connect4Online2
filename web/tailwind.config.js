98231d7b47dfb0ec24fac5f99ef155358d3cf8b4
james <71916438+jamesie@users.noreply.github.com>
2022-10-06 18:19:52 +1030

    landing page card

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+      },
+      animation: {
+        bounce2: "bounce2 1s ease 0s infinite normal forwards"
+      },
+    },
   },
   plugins: [],
-}
+};
