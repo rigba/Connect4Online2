cd4e07ec12e1b56eb1a1b5ec51688c2bd8f870a9
james <71916438+jamesie@users.noreply.github.com>
2022-10-06 18:23:28 +1030

    add landing card to landing page to inde

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,11 +1,18 @@
+import { PlusIcon, TrashIcon } from "@heroicons/react/20/solid";
 import type { NextPage } from "next";
+import Lobby from "../components/Landing/Lobby";
 import Taskbar from "../components/Taskbar";
+
+
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className="mx-4">
       <Taskbar />
-    </>
+      <div className="flex justify-center my-10 gap-5 rounded-md bg-slate-800 max-w-4xl mx-auto py-10 group inner-shadow-lg " >
+        <Lobby/>
+      </div>
+    </div>
   );
 };
 
