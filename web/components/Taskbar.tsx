88d2254b9a1722502831cb2d0f06edee5859b4b8
james <71916438+jamesie@users.noreply.github.com>
2022-10-07 17:22:27 +1030

    added user callback and dummy response for logic

--- web/components/Taskbar.tsx
+++ web/components/Taskbar.tsx
@@ -5,8 +5,18 @@ import {
   TrashIcon,
   PlusIcon,
 } from "@heroicons/react/20/solid";
+import { useEffect, useState } from "react";
 
-const Taskbar = () => {
+export const myUser = {id: 1, username:"bob"};
+
+const Taskbar = ({ passUser }) => {
+  const [user, setUser] = useState<any>();
+
+  useEffect(() => {
+    setUser(myUser);
+    passUser(user);
+  }, [user]);
+
   return (
     <div className="flex rounded-xl mx-auto max-w-5xl my-5 shadow-lg bg-slate-700 p-4 px-9">
       <text className="text-4xl public-xl text-slate-100 mr-auto">
@@ -36,28 +46,37 @@ const Taskbar = () => {
             >
               <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 text-black">
                 <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                  <div className="relative bg-white p-7 lg:grid-cols-2">
-                    {`Welcome back {User}! `}
-                  </div>
-
-                  <div className="relative grid gap-4 bg-white p-7 lg:grid-cols-2">
-                    <button className="flex center text-blue-600 outline rounded-lg w-full ring-0 focus:ring-8 ring-blue-300">
-                      <PencilSquareIcon
-                        /* for editing name */
-                        className="text-opacity-70 h-8 w-8 my-auto mx-2"
-                        aria-hidden="true"
-                      />
-                      Username Change
-                    </button>
-                    <button className="flex center text-rose-600 outline rounded-lg ring-0 focus:ring-8 ring-rose-300">
-                      <TrashIcon
-                        /* for deleting account */
-                        className="text-opacity-70 h-8 w-8 my-auto mx-2"
-                        aria-hidden="true"
-                      />
-                      Delete Account
-                    </button>
-                  </div>
+                  {user === undefined ? (
+                    <>
+                      <div className="relative bg-white p-7 lg:grid-cols-2">
+                        You are currently not logged in!
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <div className="relative bg-white p-7 lg:grid-cols-2">
+                        {`Welcome back ${user.username.toUpperCase()}! `}
+                      </div>
+                      <div className="relative grid gap-4 bg-white p-7 lg:grid-cols-2">
+                        <button className="flex center text-blue-600 outline rounded-lg w-full ring-0 focus:ring-8 ring-blue-300">
+                          <PencilSquareIcon
+                            /* for editing name */
+                            className="text-opacity-70 h-8 w-8 my-auto mx-2"
+                            aria-hidden="true"
+                          />
+                          Username Change
+                        </button>
+                        <button className="flex center text-rose-600 outline rounded-lg ring-0 focus:ring-8 ring-rose-300">
+                          <TrashIcon
+                            /* for deleting account */
+                            className="text-opacity-70 h-8 w-8 my-auto mx-2"
+                            aria-hidden="true"
+                          />
+                          Delete Account
+                        </button>
+                      </div>
+                    </>
+                  )}
                 </div>
               </Popover.Panel>
             </Transition>
