01cc16b5be7ec4ae00083ed9c5b92935901ff554
james <71916438+jamesie@users.noreply.github.com>
2022-11-07 22:08:24 +1030

    added subscripton to gamepage, partially hooked up api to ui
    need to create move funciton

--- web/pages/game/[gameId].tsx
+++ web/pages/game/[gameId].tsx
@@ -1,92 +1,131 @@
-import type { NextPage } from "next";
+import type { NextPage, NextPageContext } from "next";
 import Taskbar from "../../components/Taskbar";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import {
+  MutationResult,
+  QueryResult,
+  SubscriptionResult,
+  useLazyQuery,
+  useQuery,
+  useSubscription,
+} from "@apollo/client";
+import {
+  CreateGameMutation,
+  FetchGameDocument,
+  GameInfoDocument,
+  GameInfoSubscription,
+  MeQuery,
+} from "../../graphql/generated/graphql";
+import GameStateUI from "../../components/GameStateUI";
 
-enum gameStates {
-  CREATED,
-  JOINED,
-  USER1TURN,
-  USER2TURN,
-  COMPLETED,
-}
+const initBoard = [
+  "0000000",
+  "0000000",
+  "0000000",
+  "0000000",
+  "0000000",
+  "0000000",
+]
 
-const res = {
-  gameboard: [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]],
-  users: [{ id: 1, name: "bob" }, { id: 2, username: "carl" }],
-  whosTurn: 1,
-  winner: 0
-}
-
-const myCol = 1
-
-
-
-const Game: NextPage = () => {
-  const [gameBoard, setGameBoard] = useState([[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 2, 0], [0, 0, 1, 2, 0, 1, 0]],)
-  const [isTurn, setIsTurn] = useState(0)
+const Game: NextPage = (xd) => {
   const [hovered, setHovered] = useState<[number, number]>([9, 9]);
-  const router = useRouter()
-  const { gameId } = router.query
-  const [gameState, setGameState] = useState<gameStates>(gameStates.CREATED)
-  const [timer, setTimer] = useState({ timePassed: 0, stroke: "283", active: false  });
+  const [timer, setTimer] = useState({
+    timePassed: 0,
+    stroke: "283",
+    active: false,
+  });
+  const router = useRouter();
+  const { gameId } = router.query;
+  const [userRes, setUserRes] = useState<QueryResult<MeQuery>>();
+  const [gameQuery, gameInfo] = useLazyQuery(FetchGameDocument, {
+    ssr: false,
+    variables: { gameId },
+  });
+  useEffect(() => {
+    if (!gameId) return;
+    const subscribe = async () => {
+      await gameQuery({ variables: { gameId: gameId as string } });
+      gameInfo.subscribeToMore({
+        document: GameInfoDocument,
+        variables: { gameUUID: gameId as string },
+        updateQuery: (prev, { subscriptionData }) => {
+          if (!subscriptionData.data) return prev;
+          return Object.assign({}, prev);
+        },
+      });
+    };
+    subscribe()
+  }, [gameId]);
 
   const findDrop = (column): [number, number] => {
-    for (let i = 0; i < gameBoard.length; i++) {
-      if (gameBoard[i][column] !== 0) {
-        return [i - 1, column]
+    for (let i = 0; i < initBoard.length; i++) {
+      if (initBoard[i].charAt(column) !== "0") {
+        return [i - 1, column];
       }
     }
-    return [gameBoard.length - 1, column]
-  }
-
-  function startTimer() {
-
-    let time = setInterval(() => {
-      if (timer.active) return 
-
-      setTimer({ stroke: ((15 - timer.timePassed + 1) / 15 * 283).toFixed(0), timePassed: timer.timePassed += 1, active: true });;
-      
-      if (timer.timePassed >= 15) {
-        setTimer({ timePassed: 0, stroke: "283", active: false });
-        clearInterval(time)
-        return;
-      }
-    }, 1000);
-  }
-
-
+    return [initBoard.length - 1, column];
+  };
 
   return (
-    <div className="mx-4">
-      <Taskbar />
-      <div className="flex flex-wrap flex-row justify-center gap-5 rounded-lg bg-gray-700 max-w-4xl mx-auto p-5 group shadow-2xl" onClick={() => console.log(hovered)}>
-        <div className="gameBoard w-full max-w-lg">
-          {gameBoard.map((val, i) => {
-            const row = val.map((val2, j) => {
-
+    <div className="mx-4 h-screen overflow-auto">
+      <Taskbar passUser={setUserRes} />
+      <div className="p-0 md:p-2" />
+      <div
+        className="flex flex-wrap flex-row justify-center gap-5 rounded-lg bg-gray-700 max-w-5xl mx-auto px-5 py-12 group shadow-2xl my-auto"
+        onClick={() => console.log(gameInfo)}
+      >
+        <div className="initBoard w-full max-w-lg">
+          {initBoard.map((val, i) => {
+            const row = val.split("").map((val2, j) => {
               return (
-                <div className="inline-block relative z-0 tile-stroke" key={`${i}-${j}-grid`}>
-                  <svg className={`
+                <div
+                  className="inline-block relative z-0 tile-stroke"
+                  key={`${i}-${j}-grid`}
+                >
+                  <svg
+                    className={`
                   ${i == 0 && j == 0 ? "rounded-tl-2xl" : null}
                   ${i == 0 && j == 6 ? "rounded-tr-2xl" : null} 
                   ${i == 5 && j == 0 ? "rounded-bl-2xl" : null} 
                   ${i == 5 && j == 6 ? "rounded-br-2xl" : null} 
-                  absolute w-full h-full`} viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg" onMouseEnter={() => setHovered(findDrop(j))}>
-                    <path d="M-1.047-1.397H500.35V500H-1.047V-1.397Zm253.506 48.643c-112.112 0-202.993 90.881-202.993 202.993 0 112.106 90.881 202.987 202.993 202.987 112.111 0 202.992-90.881 202.992-202.987 0-112.112-90.881-202.993-202.992-202.993Z" className="fill-blue-700" />
+                  absolute w-full h-full`}
+                    viewBox="0 0 500 500"
+                    xmlns="http://www.w3.org/2000/svg"
+                    onMouseEnter={() => setHovered(findDrop(j))}
+                  >
+                    <path
+                      d="M-1.047-1.397H500.35V500H-1.047V-1.397Zm253.506 48.643c-112.112 0-202.993 90.881-202.993 202.993 0 112.106 90.881 202.987 202.993 202.987 112.111 0 202.992-90.881 202.992-202.987 0-112.112-90.881-202.993-202.992-202.993Z"
+                      className="fill-blue-700"
+                    />
                   </svg>
 
                   <svg viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
-                    <ellipse className={`${val2 === 0 ? (hovered[0] === i && hovered[1] === j ? `hover-ani ${myCol == 1 ? "fill-red-500" : "fill-yellow-500"}` : "fill-blue-900") 
-                    : val2 === 1 ? "fill-red-500" : "fill-yellow-500"}`} cx="250.349" cy="248.603" rx="248.953" ry="248.953" />
+                    <ellipse
+                      className={
+                        val2 === "0"
+                          ? hovered[0] === i && hovered[1] === j
+                            ? `hover-ani ${
+                                gameInfo.data?.fetchGame?.createdId ==
+                                userRes?.data?.me?.id
+                                  ? "fill-red-500"
+                                  : "fill-yellow-500"
+                              }`
+                            : "fill-blue-900"
+                          : val2 === "1"
+                          ? "fill-red-500"
+                          : "fill-yellow-500"
+                      }
+                      cx="250.349"
+                      cy="248.603"
+                      rx="248.953"
+                      ry="248.953"
+                    />
                   </svg>
-
-
                 </div>
-              )
-
-            })
-            return row
+              );
+            });
+            return row;
           })}
         </div>
         <div className="rounded-xl mx-4 shadow-lg bg-gray-600 p-4 px-9 my-auto" >
